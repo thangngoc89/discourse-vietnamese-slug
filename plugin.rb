8ab9b6b77a992baa1fafbea9aefaa4edcf9ee267
thangngoc89 <thangngoc89@users.noreply.github.com>
2015-01-11 00:11:36 +0700

    Initial

---
+++ plugin.rb
@@ -0,0 +1,28 @@
+# author: Khoa Nguyen
+# name: Discourse Vietnamese Slug
+# version: 1.0
+
+module Slug
+
+  def self.for(string)
+  
+	#For Vietnamese slug 
+    vietnamese   = "àáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴÈÉẸẺẼÊỀẾỆỂỄÌÍỊỈĨÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠÙÚỤỦŨƯỪỨỰỬỮỲÝỴỶỸĐêùà"
+    replacements = "aaaaaaaaaaaaaaaaaeeeeeeeeeeeiiiiiooooooooooooooooouuuuuuuuuuuyyyyydAAAAAAAAAAAAAAAAAEEEEEEEEEEEIIIIIOOOOOOOOOOOOOOOOOUUUUUUUUUUUYYYYYDeua"
+	  string = string.tr(vietnamese, replacements)
+	#End Vietnamese slug
+	
+    slug = string.gsub("'", "").parameterize
+    slug.gsub!("_", "-")
+    # TODO review if ja should use this
+    # ko asked for it to be removed
+    if ['zh_CN', 'ja'].include?(SiteSetting.default_locale)
+      unless defined? Stringex
+        require 'stringex_lite'
+      end
+      slug = string.to_url
+    end
+    slug =~ /[^\d]/ ? slug : '' # Reject slugs that only contain numbers, because they would be indistinguishable from id's.
+  end
+
+end
